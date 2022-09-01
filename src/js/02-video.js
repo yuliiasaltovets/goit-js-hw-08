db604ec374fe6ed8076b2878a47c42baeded12a9
yuliiasaltovets <103329740+yuliiasaltovets@users.noreply.github.com>
2022-09-01 15:12:25 +0200

    01.09

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,43 @@
+import '../css/common.css';
+import Player from '@vimeo/player';
+import throttle from "lodash.throttle";
 
+const VIMEO_KEY = 'videoplayer-current-time';
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+
+function onPlay (evt) {
+    const stringifyOnPlay = JSON.stringify(evt);
+    localStorage.setItem(VIMEO_KEY, stringifyOnPlay)
+}
+
+   player.on('timeupdate', throttle(onPlay, 1000));
+
+function playerBack() {
+    if (JSON.parse(localStorage.getItem(VIMEO_KEY)) === null){
+        return;
+    }
+    const paused = JSON.parse(localStorage.getItem(VIMEO_KEY))['seconds'];
+
+    if (paused) {
+        player
+        .setCurrentTime(paused)
+        .then(function (seconds){})
+        .catch(function (error){
+            switch (error.name) {
+                case 'Error':
+                    break;
+                    default:
+                    break;
+            }
+        })
+    }
+}
+
+playerBack();
+
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,48 @@
+import throttle from "lodash.throttle";
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const feedbackForm = document.querySelector('.feedback-form');
+feedbackForm.addEventListener('change', throttle(initForm, 500))
+
+
+initForm();
+
+feedbackForm.addEventListener('submit', evt => {
+    evt.preventDefault();
+    evt.target.reset(localStorage.removeItem(STORAGE_KEY));
+
+    const formData = new FormData(feedbackForm)
+    formData.forEach((value, name) => console.log(value, name));
+});
+
+feedbackForm.addEventListener('change', evt => {
+
+    let persistedForm = localStorage.getItem(STORAGE_KEY);
+    persistedForm = persistedForm ? JSON.parse(persistedForm) : {};
+    persistedForm[evt.target.name] = evt.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(persistedForm));
+   
+  
+
+   
+});
+ 
+
+function initForm (){
+    let persistedForm = localStorage.getItem(STORAGE_KEY);
+    
+    if (persistedForm) {
+        persistedForm = JSON.parse(persistedForm)
+        
+        Object.entries(persistedForm).forEach(([name, value]) => {
+            feedbackForm.elements[name].value = value;
+        });
+    }
+};
+
+
+
+
+
 
